8690e63be7b2dd2f82358de97dde1e7977406fce
ikhana <inaa.eth@gmail.com>
2026-06-14 02:51:50 +0500

    step 04.1 — Media upload config with image sizes and focal point

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -12,5 +12,29 @@ export const Media: CollectionConfig = {
       required: true,
     },
   ],
-  upload: true,
+  upload: {
+    mimeTypes: ['images/*'],
+    imageSizes: [
+      {
+        name: 'thumbnail',
+        height: 400,
+        width: 300,
+        position: 'centre',
+      },
+      {
+        name: 'card',
+        height: 768,
+        width: 1024,
+      },
+      {
+        name: 'og',
+        height: 1200,
+        width: 630,
+        position: 'centre',
+      },
+    ],
+    adminThumbnail: 'adminthumbnail',
+    focalPoint: true,
+    crop: true,
+  },
 }
